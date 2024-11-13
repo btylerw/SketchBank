6f56bff968b161ad03e182422fb2c954d3d4b118
Tyler Brown <brown.tyler.w@gmail.com>
2024-11-12 23:33:52 -0800

    Added account balance to home page and button to allow user to change balance

--- sketch-bank-frontend/src/HomePage.jsx
+++ sketch-bank-frontend/src/HomePage.jsx
@@ -4,9 +4,27 @@ import { useLocation } from "react-router-dom";
 export const HomePage = () => {
     const location = useLocation();
     const username = location.state.username;
+    const [balance, setBalance] = useState(0);
+    const [showForm, setShowForm] = useState(false);
+
+    function changeInputVisibility() {  
+        setShowForm(!showForm);
+    }
+
+    function handleBalanceChange(e) {
+        setBalance(Number(e.target.newBalance.value));
+        changeInputVisibility();
+    }
     return (
         <>
-        <h1>Hello {username}!</h1>
+            <h1>Hello {username}!</h1>
+            <h2>You have ${balance}</h2>
+            <button onClick={changeInputVisibility}>Change Balance</button>
+            {showForm &&
+            <form action="" onSubmit={handleBalanceChange}>
+                Balance: <input name="newBalance" type="text" />
+                <input type="submit" value="Enter" />
+            </form>}
         </>
     )
 }
